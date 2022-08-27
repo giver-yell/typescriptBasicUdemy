f1e4922e68e80c7b811fc57b6ad916b7f97fb2a2
Ryohei Fujihara <giver.yell@gmail.com>
2022-08-27 14:11:36 +0900

    enum

---
+++ src/enum.ts
@@ -0,0 +1,28 @@
+export {};
+
+enum Months {
+  January,
+  February,
+  March,
+}
+
+console.log(Months.January);
+
+enum Week {
+  Sunday = 1,
+  Monday,
+}
+console.log(Week.Monday);
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = '#FFFFF',
+  //   YELLOW = '#FFFF00',
+}
+
+enum COLORS {
+  YELLOW = '#FFFF00',
+  // GRAY
+}
+
+console.log(COLORS.RED);
